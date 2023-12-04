2992a3d8db355928f5201dae0e103247aa272ad6
Boris <146715808+XeUby@users.noreply.github.com>
2023-12-05 01:02:47 +0200

    Update RamenChef.cs

--- BuilderAssignment/RamenChef.cs
+++ BuilderAssignment/RamenChef.cs
@@ -1,5 +1,12 @@
-﻿using BuilderAssignment.Builders;
+using BuilderAssignment.Builders;
 using BuilderAssignment.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BuilderAssignment
 {
@@ -8,8 +15,15 @@ namespace BuilderAssignment
         public RamenBowl PrepareRamen(IRamenBuilder builder)
         {
             //TODO#1: Call each builder step declared in RamenBuilder class
-            
-            return null; //TODO#2: Finaly return build bowl of ramen
+            builder.settingBroth();
+            builder.settingNoodle();
+            builder.addTopping();
+            builder.addExtra();
+            return builder.GetRamen() ; //TODO#2: Finaly return build bowl of ramen
         }
     }
 }
+
+//In a complete implementation, i will have concrete classes that implement the IRamenBuilder interface. 
+//Each concrete builder would provide its own implementation for setting up the ramen bowl according to specific recipes or preferences. 
+//The RamenChef class would then use the builder to create different variations of ramen bowls.
