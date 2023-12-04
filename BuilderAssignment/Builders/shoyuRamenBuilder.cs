e22e99003d109102a1aebb8cc622c907d95e0aeb
Boris <146715808+XeUby@users.noreply.github.com>
2023-12-05 01:36:48 +0200

    Update shoyuRamenBuilder.cs

--- BuilderAssignment/Builders/shoyuRamenBuilder.cs
+++ BuilderAssignment/Builders/shoyuRamenBuilder.cs
@@ -7,32 +7,43 @@ using System.Threading.Tasks;
 
 namespace BuilderAssignment.Builders
 {
-    internal class shoyuRamenBuilder : IRamenBuilder
+    // Concrete builder for creating Shoyu Ramen
+    internal class ShoyuRamenBuilder : IRamenBuilder
     {
+        // Instance of the RamenBowl being constructed
         private RamenBowl ramen = new RamenBowl();
+
+        // Set the type of broth for Shoyu Ramen
         public void settingBroth()
         {
-            ramen.Broth = BrothType.shoyu;
+            ramen.Broth = BrothType.Shoyu;
         }
 
+        // Set the type of noodle for Shoyu Ramen
         public void settingNoodle()
         {
             ramen.Noodle = NoodleType.Soba;
         }
+
+        // Add specific toppings for Shoyu Ramen
         public void addTopping()
         {
             ramen.Topping.Add(Topping.BambooShoots);
             ramen.Topping.Add(Topping.Seaweed);
         }
+
+        // Shoyu Ramen doesn't have extra components, set extras to null
         public void addExtra()
         {
             ramen.Extra = null;
         }
 
+        // Return the completed Shoyu Ramen bowl
         public RamenBowl GetRamen()
         {
             return ramen;
         }
-
     }
+}
+
 }
